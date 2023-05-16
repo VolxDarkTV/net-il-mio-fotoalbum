4af85b9e3dddfcb473d3a4747e7af482046fb985
Vincenzo FOTI <vincenzo.foti@outlook.it>
2023-05-16 22:10:17 +0200

    fixed img and add controll on description

--- FinalProject/wwwroot/js/Client/index.js
+++ FinalProject/wwwroot/js/Client/index.js
@@ -14,29 +14,36 @@ function loadPosts() {
                 document.getElementById('y-posts').innerHTML = '';
                 res.data.forEach(post => {
                     console.log('Post', post);
+                    let descriptionHTML = '';
+                    if (post.description != null) {
+                        descriptionHTML = `<span><span class="fw-bold">Description: </span>${post.description}</span>`
+                    }
                     document.getElementById('y-posts').innerHTML += 
                     `
                         <li class="d-flex gap-3">
-                        <div>
+                        
                             <!--Image-->
                             <div class="card_image rounded">
-                                <img class="rounded" src="${post.img}" alt="Error: ${post.title}">
+                                <img class="my_img rounded" src="${post.img}" alt="Error: ${post.title}">
                             </div>
-                        </div>
+                        
 
-                        <div class="d-flex flex-column align-items-center justify-content-around gap-3 w-100 border border-1 border-warning">
+                            <div class="d-flex flex-column align-items-center justify-content-around gap-3 w-100 border border-1 border-warning">
+                            
+                                <div class="d-flex flex-column">
+                                    <span><span class="fw-bold">Title: </span>${post.title}</span>
+                                    ${descriptionHTML}
+                                </div>
 
-                            <div class="d-flex flex-column">
-                                <span><span class="fw-bold">Title: </span>${post.title}</span>
-                                <span><span class="fw-bold">Description: </span>${post.description}</span>
+                                <div class="d-flex gap-3">
+                                    <!--Details-->
+                                    <a class="btn btn-dark shadow rounded-pill py-2 px-3" href="/Client/Detail?id=${post.id}">
+                                        <i class="fa-regular fa-eye"></i>
+                                    </a>
+                                </div>
                             </div>
 
-                            <div class="d-flex gap-3">
-                                <!--Details-->
-                                <a class="btn btn-dark shadow rounded-pill py-2 px-3" href="/Client/Detail?id=${post.id}">
-                                    <i class="fa-regular fa-eye"></i>
-                                </a>
-                            </div>
+                        </li>
                     `
                 })
             }
